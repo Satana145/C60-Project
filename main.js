976e7112dcac8b266461d27ff83637c6aa4f5f6f
Satana145 <76929429+Satana145@users.noreply.github.com>
2021-01-16 20:53:37 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function pdfbo(){
+    document.getElementById("pdf").src = "https://file1.largepdf.com/file/2020/09/26/The_Blood_of_Olympus_-_Rick_Riordan.pdf";
+}
+function pdfhp(){
+    document.getElementById("pdf").src = "https://mcqueenglish.weebly.com/uploads/3/2/2/6/32260387/harry_potter_and_the_goblet_of_fire.pdf";
+}
